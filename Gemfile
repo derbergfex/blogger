ab8625749b031cf04ea84cbb467b2b147ea87b2b
Yussef Soudan <yussefsoudan@gmail.com>
2018-08-02 20:48:00 +0100

    Sorcery authentication complete

--- Gemfile
+++ Gemfile
@@ -2,7 +2,8 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.3.4'
-
+gem 'sorcery'
+gem 'bcrypt-ruby'
 gem "paperclip"
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,4 +1,6 @@
 class TagsController < ApplicationController
+
+    before_action :require_login, only: [:destroy]
     def show
         @tag = Tag.find(params[:id])
     end
@@ -14,3 +16,5 @@ class TagsController < ApplicationController
         redirect_to tags_path
     end
 end
+
+
